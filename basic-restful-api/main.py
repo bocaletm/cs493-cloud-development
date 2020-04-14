57db012642550b97cc974edc28d096945bc30699
Mario <mario.bocaletti@ge.com>
2020-04-14 13:27:01 -0500

    add patch

--- basic-restful-api/main.py
+++ basic-restful-api/main.py
@@ -57,8 +57,27 @@ def get_boat(boat_id, baseUri):
         boat.update({"self":selfUri})
         return boat
 
-def update_boat(boat_id):
-    return 200
+def update_boat(baseUri, boat_id, name, boatType, length):
+    query = datastore_client.query(kind='Boat')
+    boat_key = datastore_client.key('Boat', int(boat_id))
+    query.key_filter(boat_key)
+    boats = query.fetch()
+    for boat in boats:
+        id = boat.key.id_or_name
+        selfUri = baseUri + str(id)
+        boat.update({
+            "id": id,
+            "name": name, 
+            "type": boatType, 
+            "length": length,
+            "self": selfUri
+        })
+        datastore_client.put(boat)
+        boat.update({
+            "id": id,
+            "self": selfUri
+        })        
+        return boat    
 
 def delete_boat(boat_id):
     return 200
@@ -86,7 +105,7 @@ app = Flask(__name__)
 @app.route('/boats', methods =['POST']) 
 def postBoat():
     content = request.json
-    if content.get('name',None) == None or content.get('type',None) == None or content.get('length',None) == None:
+    if content == None or content.get('name',None) == None or content.get('type',None) == None or content.get('length',None) == None:
         return errorResponse(400,'The request object is missing at least one of the required attributes')
     id = store_boat(content['name'],content['type'],content['length']) 
     if id is not -1:
@@ -118,8 +137,15 @@ def getBoat(boat_id):
 
 @app.route('/boats/<string:boat_id>', methods =['PATCH']) 
 def updateBoat(boat_id):
-    status = update_boat(boat_id) 
-    return Response(status=status, mimetype='application/json')
+    content = request.json
+    if content == None or content.get('name',None) == None or content.get('type',None) == None or content.get('length',None) == None:
+        return errorResponse(400,'The request object is missing at least one of the required attributes')
+    boat = update_boat(request.base_url, boat_id, content['name'], content['type'], content['length']) 
+    if boat is not None:
+        print(boat)
+        return make_response(jsonify(boat), 200)
+    else: 
+        return errorResponse(404,'No boat with this boat_id exists')
 
 @app.route('/boats/<string:boat_id>', methods =['DELETE']) 
 def deleteBoat(boat_id):
