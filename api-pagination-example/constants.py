56af03378d4e99854d349467da4a87942ded8a1e
Mario <mario.bocaletti@ge.com>
2020-04-25 18:50:47 -0500

    clean constants

--- api-pagination-example/constants.py
+++ api-pagination-example/constants.py
@@ -5,9 +5,7 @@ class Constants:
     NO_LOAD_OR_BOAT = 'The specified boat_id and/or load_id don’t exist'
     SLIP_OCCUPIED = 'The slip is not empty'
     ALREADY_ASSIGNED = 'Load already assigned'
-    DOCKING_NOT_FOUND = 'No boat with this boat_id is at the slip with this slip_id'
     INCOMPLETE = 'The request object is missing at least one of the required attributes'
-    INCOMPLETE_SLIP = 'The request object is missing the required number'
     INVALID_DATA = 'Error: invalid data in post'
-    DOCUMENTATION = 'https://canvas.oregonstate.edu/courses/1764544/files/79296197/download'
+    DOCUMENTATION = ''
     limit = 3
