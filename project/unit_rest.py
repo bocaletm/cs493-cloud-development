079975d116e7be0d2b32e33859caf82d6bdc1fc7
Mario <mario.bocaletti@ge.com>
2020-06-02 09:56:08 -0500

    add null legion to unit post resp

--- project/unit_rest.py
+++ project/unit_rest.py
@@ -38,6 +38,7 @@ def post():
         content.update({"id":id})
         content.update({"self": request.base_url + '/' + str(id)})
         content.update({"owner": userId })
+        content.update({"legion": None })
         content.update({"cost": unit.getCost(content['strength'], content['targetRange']) })
         return R.contentResponse(201,content)
     else:
